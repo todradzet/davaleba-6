e210f72fed6a6890ca8384784e7e2140e0dce887
todradzet <tamta.todradze.1@iliauni.edu.ge>
2024-08-24 19:33:14 +0400

    davaleba 6

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const array = [10, 20, 30, 40, 50];
+const valueToFind = 30;
+
+if (array.includes(valueToFind)) {
+    console.log("რიცხვი ნაპოვნია");
+} else {
+    console.log("რიცხვი არ მოიძებნა");
+}
